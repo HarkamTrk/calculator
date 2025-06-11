3d3d59e425faa4a11fd3c282c2812aec6971cdd1
HarkamTrk <hichamtrk07@gmail.com>
2025-06-11 16:02:32 +0200

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -19,7 +19,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(multiply(-1, 1), -1)
         self.assertEqual(multiply(-1, -1), 1)
 
- def test_divide(self):
+    def test_divide(self):
         self.assertEqual(divide(1, 2), 0.5)
         self.assertEqual(divide(-1, 1), -1)
         self.assertEqual(divide(-1, -1), 1)
